5e24509537917b227e8a37fb48c1b31be5935135
aesha98 <sityasya@gmail.com>
2024-05-19 13:47:12 +0800

    build object and event listener

--- js/books.js
+++ js/books.js
@@ -1,6 +1,12 @@
 /*List of books*/
 const myLibrary = [];
 
+document.querySelector("button").addEventListener("click",function() {
+	 addBookToLibrary('Jarry Potter', 'J.K Rowling', 135, true);
+	 console.log(myLibrary);
+});
+
+
 /* constructor */
 function Book(title, author, numOfPages, read_status) {
 	this.title = title;
@@ -13,4 +19,10 @@ function Book(title, author, numOfPages, read_status) {
 function addBookToLibrary(title, author, numOfPages, read_status) {
 	let newBook = new Book(title, author, numOfPages, read_status);
 	myLibrary.push(newBook);
+	return newBook;
 }
+
+addBookToLibrary('Harry Potter', 'J.K Rowling', 135, true);
+addBookToLibrary('Lesson In Chemistry', 'J.R', 90, true);
+
+console.log(myLibrary);
